4b52597c6d530d4b5d2e83cbdfbca3ea8755a588
Vasili-Sk <1153192+Vasili-Sk@users.noreply.github.com>
2023-04-01 17:00:03 +0600

    Release build fixes

--- LEVCAN Configurator/Properties/Resources.Designer.cs
+++ LEVCAN Configurator/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LEVCANsharpTest.Properties {
+namespace LEVCANConfigurator.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace LEVCANsharpTest.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LEVCANsharpTest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LEVCANConfigurator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
